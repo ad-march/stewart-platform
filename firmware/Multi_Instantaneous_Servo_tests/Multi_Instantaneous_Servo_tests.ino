400b3531ab639b12493f441d9dd4a88e9f5e3a49
acourville <44624553+acourville@users.noreply.github.com>
2018-11-06 02:03:08 -0500

    Update firmware/Multi_Instantaneous_Servo_tests/Multi_Instantaneous_Servo_tests.ino

--- firmware/Multi_Instantaneous_Servo_tests/Multi_Instantaneous_Servo_tests.ino
+++ firmware/Multi_Instantaneous_Servo_tests/Multi_Instantaneous_Servo_tests.ino
@@ -7,10 +7,13 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMAX  535
 
 int servonum = 0;
-int servo_quantity = 2;
-int speed = 5;
-double current_angle [] = {150,150};
-double target_angle [] = {250, 500};
+const int servo_quantity = 6;
+const int steps_quantity = 3;
+double speed = 0.5;
+double current_angle [] = {150,150,150,535,535,535};
+double target_angle [][servo_quantity] = {{250, 500, 490, 400, 200, 300},
+                                          {210, 400, 150, 400, 500, 425},
+                                          {500, 300, 350, 500, 300, 350}};
 
 void setup() {
   Serial.begin(9600);
@@ -22,50 +25,55 @@ void setup() {
 
   for (int a = 0; a < servo_quantity; a++){
     pwm.setPWM(a, 0, current_angle[a]);
-    delay(500);
+    Serial.flush();
   }
 
-  delay(10);
+  delay(500);
 }
 
 void loop() {
 
-//Finds servo having to move the most
-double max_diff = 0;
-int servonum_max;
-  
-  for (int i = 0; i < servo_quantity; i++) {
-    double diff = abs(target_angle[i]-current_angle[i]);
+for (int b = 0; b < steps_quantity; b++){  
+
+  //Finds servo having to move the most
+  double max_diff = 0;
+  int servonum_max;
     
-    if (diff > max_diff){
-      max_diff=diff;
-      servonum_max = i;
+    for (int i = 0; i < servo_quantity; i++) {
+      double diff = abs(target_angle[b][i]-current_angle[i]);
+      
+      if (diff > max_diff){
+        max_diff=diff;
+        servonum_max = i;
+      }
     }
-  }
-
-// Split into step sizes for same total move time
-double step_size [servo_quantity];
- for (int j = 0; j < servo_quantity; j++) {
-  step_size[j] = (abs(target_angle[j]-current_angle[j]))/max_diff;
- }
-
-
-//makes stuff move
-
-while(current_angle[servonum_max] < target_angle[servonum_max]){
- 
- for (int k = 0; k < servo_quantity; k++){
   
-  current_angle[k] = current_angle[k] + step_size[k];
-  //double pulselen = map (current_angle[k], 0, 180, SERVOMIN, SERVOMAX);
-  //pwm.setPWM(k, 0, pulselen);
-  pwm.setPWM(k, 0, current_angle[k]);
-  delay(speed);
+  // Split into step sizes for same total move time
+  double step_size [servo_quantity];
+   for (int j = 0; j < servo_quantity; j++) {
+    step_size[j] = (target_angle[b][j]-current_angle[j])/max_diff;
+   }
+  
+  
+  //makes stuff move
+  
+  while(current_angle[servonum_max] < target_angle[b][servonum_max]){
+   
+   for (int k = 0; k < servo_quantity; k++){
+    
+    current_angle[k] = current_angle[k] + step_size[k];
+    //double pulselen = map (current_angle[k], 0, 180, SERVOMIN, SERVOMAX);
+    //pwm.setPWM(k, 0, pulselen);
+    pwm.setPWM(k, 0, current_angle[k]);
+    delay(speed);
+  
+    Serial.flush();
+   }
+  }
+  }
+}
 
-  Serial.flush();
- }
-}
-}
+
 
 
 
