534b397f9a80dbe35455ed4c5d86c72c2bc9d074
acourville <44624553+acourville@users.noreply.github.com>
2018-11-22 18:01:55 -0500

    Update Servo_Calibration.ino

--- firmware/Servo_Calibration/Servo_Calibration.ino
+++ firmware/Servo_Calibration/Servo_Calibration.ino
@@ -5,7 +5,7 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 //Servo calibration:
 int SERVOMIN[] = {300,410,225,410,240,415,230};
 int SERVOMAX[] = {575,210,520,215,530,210,530};
-int SERVOMEAN[] = {480,390,350,390,350,380,350};
+int SERVOMEAN[] = {445,390,350,390,350,400,360};
 
 
 void setup() {
@@ -17,17 +17,17 @@ void setup() {
 void loop() {
   
 //Mean position
-   pwm.setPWM(0, 0, SERVOMAX[0]);
-      Serial.flush();
+  // pwm.setPWM(0, 0, SERVOMEAN[0]);
+    //  Serial.flush();
 
-/*
+
   for (int i = 0; i < 6; i++){
-      pwm.setPWM(i+1, 0, SERVOMAX[i]);
+      pwm.setPWM(i+1, 0, SERVOMEAN[i+1]);
       Serial.flush();
     }
 
 delay(1000);
-
+/*
 //Max position
   for (int i = 0; i < 6; i++){
       pwm.setPWM(i+1, 0, SERVOMAX[i]);
